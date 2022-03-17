928af8ab658ee86a707e738bed1babc94ac97918
Cannan Schaefer <cannansch@gmail.com>
2022-03-16 20:38:54 -0500

    Add files via upload

---
+++ raven_reformatted.py
@@ -0,0 +1,255 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import random
+from datetime import date
+import webbrowser as wb
+import pyautogui as py
+from time import sleep as z
+from requests_html import HTMLSession
+from datetime import datetime
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[1].id)
+
+# Greeting when ran
+raven_greetings = [
+    "at the ready boss",
+    "what can I do for you today",
+    "How can i help you today",
+    "greetings creator",
+]
+hello = random.choice(raven_greetings)
+
+# This helps you know a command was received
+raven_command_responses = [
+    "right away boss",
+    "will do boss",
+    "will do",
+    "sure thing boss",
+    "executing now",
+]
+random_response = random.choice(raven_command_responses)
+
+today = date.today()
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print("listening...")
+            voice = listener.listen(source, phrase_time_limit=15, timeout=3600)
+            command = "waiting"
+            command = listener.recognize_google(voice)
+            command = command.lower()
+    except:
+        pass
+    return command
+
+
+# Weather Web Scraper
+s = HTMLSession()
+
+# Currently set to my city, looking for a way to auto detect user city
+query = "omaha"
+url = f"https://www.google.com/search?q=weather+{query}"
+
+r = s.get(
+    url,
+    headers={
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
+    },
+)
+temp = r.html.find("span#wob_tm", first=True).text
+weather_desc = r.html.find("div.VQF4g", first=True).find("span#wob_dc", first=True).text
+humidity = r.html.find("div.wtsRwe", first=True).find("span#wob_hm", first=True).text
+wind_speed = r.html.find("div.wtsRwe", first=True).find("span#wob_ws", first=True).text
+
+
+def run_raven():
+    command = take_command()
+
+    # Searches youtube and plays the most relevant result
+    if "raven play" in command:
+        song = command.replace("raven play", "")
+        talk("playing " + song)
+        pywhatkit.playonyt(song)
+
+    elif "raven" in command and "time" in command:
+        now = datetime.now()
+        time = now.strftime("%I:%M")
+        talk("Current time is " + time)
+
+    # Reads wiki articles or runs a search
+    elif "raven look up" in command:
+        try:
+            topic = command.replace("raven look up", "")
+            info = wikipedia.summary(topic.strip(), 1)
+            talk(info)
+        except wikipedia.exceptions.PageError:
+            talk(
+                "sorry boss. for some reason I didn't find any info on that. I'll run a Google search for you"
+            )
+            pywhatkit.search(topic.strip())
+            pass
+
+    elif "raven" in command and "go to sleep" in command:
+        talk("shutting down. see you later")
+        stop_raven()
+
+    elif "raven" in command and "are you there" in command:
+        talk("I am, what can i do for you")
+
+    elif "raven" in command and "date" in command:
+        talk(today)
+
+    # These link opening commands are a bit complicated due to an issue with opening the wrong browser
+    elif "raven open" in command:
+        talk(random_response)
+        website = command.replace("raven open", "")
+        link = website.replace(" ", "") + ".com"
+        wb.open("https://www.google.com")
+        z(0.15)
+        py.keyDown("ctrl")
+        py.hotkey("l")
+        z(0.15)
+        py.keyUp("ctrl")
+        z(0.15)
+        py.write(link)
+        z(0.15)
+        py.hotkey("enter")
+
+    elif "raven" in command and "power down" in command:
+        talk("powering down boss")
+        py.hotkey("win", "x")
+        z(0.25)
+        py.hotkey("u")
+        z(0.25)
+        py.hotkey("s")
+
+    elif "raven" in command and "new doc" in command:
+        talk(random_response)
+        wb.open("https://www.google.com")
+        z(0.15)
+        py.keyDown("ctrl")
+        py.hotkey("l")
+        z(0.15)
+        py.keyUp("ctrl")
+        z(0.15)
+        py.write("docs.new")
+        z(0.15)
+        py.hotkey("enter")
+
+    elif "raven" in command and "new sheet" in command:
+        talk(random_response)
+        wb.open("https://www.google.com")
+        z(0.15)
+        py.keyDown("ctrl")
+        py.hotkey("l")
+        z(0.15)
+        py.keyUp("ctrl")
+        z(0.15)
+        py.write("sheets.new")
+        z(0.15)
+        py.hotkey("enter")
+
+    # This obviously only works if the editor is pulled up
+    elif "raven" in command and "restart" in command:
+        talk("will do. see you in a moment")
+        py.hotkey("ctrl", "f5")
+
+    elif "raven google" in command:
+        talk(random_response)
+        text = command.replace("raven google", "")
+        pywhatkit.search(text.strip())
+
+    elif "raven" in command and "close tab" in command:
+        talk(random_response)
+        py.hotkey("ctrl", "w")
+
+    # Works a bit inconsistently, maybe activation phrase is too long
+    elif "raven find 3d print files of" in command:
+        talk(random_response)
+        text = command.replace("raven find 3d print files of", "")
+        wb.open("https://www.thingiverse.com/")
+        z(1.25)
+        py.hotkey("tab")
+        z(0.4)
+        py.hotkey("tab")
+        z(0.4)
+        py.write(text.strip())
+        z(0.2)
+        py.hotkey("enter")
+
+    elif "raven search youtube" in command:
+        talk(random_response)
+        text = command.replace("raven search youtube for", "")
+        wb.open("https://www.youtube.com/")
+        z(0.5)
+        py.hotkey("tab")
+        z(0.2)
+        py.hotkey("tab")
+        z(0.2)
+        py.hotkey("tab")
+        z(0.2)
+        py.write(text.strip())
+        z(0.10)
+        py.hotkey("enter")
+
+    elif "raven image search" in command:
+        talk(random_response)
+        text = command.replace("raven image search", "")
+        wb.open("https://www.google.com/imghp")
+        z(0.5)
+        py.write(text.strip())
+        z(0.10)
+        py.hotkey("enter")
+
+    elif "raven" in command and "weather" in command:
+        talk("it is " + weather_desc)
+        z(0.15)
+        talk("the current temp is " + temp)
+        z(0.3)
+
+    elif "raven type" in command:
+        talk(random_response)
+        t2t = command.replace("raven type", "")
+        py.write(t2t.strip())
+
+    # Only works with some keys at the moment
+    elif "raven" in command and "press" in command:
+        talk(random_response)
+        key = command.replace("raven press", "")
+        py.hotkey(key.strip())
+
+    elif "raven" in command and "launch" in command:
+        talk(random_response)
+        program = command.replace("raven launch", "")
+        py.hotkey("win")
+        z(0.5)
+        py.write(program.strip())
+        z(0.5)
+        py.hotkey("enter")
+
+    else:
+        print("...")
+
+
+talk(hello)
+while True:
+    try:
+        run_raven()
+    # Had an odd issue where Raven would sometimes shut down, this seemed to fix it
+    except UnboundLocalError:
+        print("Raven has stopped working")
+        talk("shutting down boss")
+        break
